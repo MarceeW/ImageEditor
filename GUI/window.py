4235492f15df92d7bfb443504c0cb55a27ff0371
MarceeW <marcelllkadar@gmail.com>
2023-10-10 21:11:30 +0200

    Window base layout

--- GUI/window.py
+++ GUI/window.py
@@ -1,5 +1,30 @@
-from tkinter import *
 import customtkinter
 
-class Window:
-    pass
+
+class Window(customtkinter.CTk):
+    def __init__(self, title, width, height, **kwargs):
+        super().__init__(**kwargs)
+        super().geometry(f'{width}x{height}')
+        super().title(title)
+
+        self.grid_rowconfigure(0, weight=1)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        padding = 5
+
+        self.controlFrame = customtkinter.CTkFrame(master=self)
+        self.controlFrame.grid(row=0, column=0, padx=padding, pady=padding, sticky="nsew")
+
+        self.imageFrame = customtkinter.CTkFrame(master=self)
+        self.imageFrame.grid(row=0, column=1, padx=padding, pady=padding, sticky="nsew")
+
+        self.blurController = customtkinter.CTkSlider(master=self.controlFrame,command=self.onBlurControllerChanged)
+        self.blurController.grid(row=0, column=1, padx=padding, pady=padding, sticky="nsew")
+
+    def onBlurControllerChanged(self,value):
+        print(value)
+
+    def start(self):
+        super().mainloop()
---
+++ main.py
@@ -0,0 +1,4 @@
+from GUI.window import Window
+
+window = Window("Image editor", 600, 600)
+window.start()
